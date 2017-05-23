dfb1a72419a4f5d180a3018f913b755279101a35
Gary A. Stafford <garystafford@rochester.rr.com>
2017-05-23 17:09:13 -0400

    Fix client gateway config

--- app/scripts/configFile.js
+++ app/scripts/configFile.js
@@ -4,7 +4,7 @@
   ng.module('voterClientApp')
     .constant('EnvironmentConfig', {
       'description': 'Call the Voter Service directly in a local development environment',
-      'apiUrl': 'http://gateway',
+      'apiUrl': 'http://api.voter-demo.com',
       'apiPort': '8080'
     });
 }(window.angular));
