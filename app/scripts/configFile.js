c57489c5cfe84e9cc252bee8bfa6ea0311353779
Gary Stafford <gary.stafford@accenture.com>
2017-11-22 10:40:28 -0500

    Changing port for API Gateway

--- app/scripts/configFile.js
+++ app/scripts/configFile.js
@@ -6,7 +6,7 @@
       // 'description': 'settings for running app locally'
       // 'apiPort': '8080',
       // 'apiUrl': 'http://localhost',
-      'description': 'settings for running app within a Docker container, in Production'
+      'description': 'settings for running app within a Docker container, in Production',
       'apiPort': '8090',
       'apiUrl': 'http://api.voter-demo.com'
     });
