b5ac521f9375175a2f5b47a1492515dc22e34722
Gary Stafford <gary.stafford@accenture.com>
2017-11-26 22:43:05 -0500

    Changing gateway port to 8080

--- app/scripts/configFile.js
+++ app/scripts/configFile.js
@@ -3,12 +3,9 @@
 
   ng.module('voterClientApp')
     .constant('EnvironmentConfig', {
-      'description': 'settings for running app locally',
-      'apiPort': '8090',
-      'apiUrl': 'http://localhost',
-      // 'description': 'settings for running app within a Docker container, in Production',
+      'apiPort': '8080',
       // 'apiPort': '8090',
-      // 'apiUrl': 'http://api.voter-demo.com',
+      'apiUrl': 'api.voter-demo.com',
       'election': '2016 Presidential Election'
     });
 }(window.angular));
