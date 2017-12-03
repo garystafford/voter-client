f84674be8c72fcb2602cf7c2a5dbe80b534845eb
Gary Stafford <gary.stafford@accenture.com>
2017-12-02 22:31:49 -0500

    Switching API to HTTPS

--- app/scripts/configFile.js
+++ app/scripts/configFile.js
@@ -3,8 +3,7 @@
 
   ng.module('voterClientApp')
     .constant('EnvironmentConfig', {
-      // 'apiPort': '8080',
-      'apiUrl': 'http://api.voter-demo.com',
+      'apiUrl': 'https://api.voter-demo.com',
       'election': '2016 Presidential Election'
     });
 }(window.angular));
